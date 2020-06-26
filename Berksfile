951d933f88311abc0c3c12e0f7088cac2f7ef87f
Bastian Schwarz <bastian@codename-php.de>
2020-06-26 12:03:39 +0200

    Initial commit

---
+++ Berksfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://supermarket.chef.io'
+source :chef_server
+
+metadata
+
+cookbook 'test', path: './test/fixtures/cookbooks/test', group: :test
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+name 'codenamephp_workstation_chef'
+maintainer 'Bastian Schwarz'
+maintainer_email 'basitan@codename-php.de'
+license 'Apache-2.0'
+description 'Wrapper cookbook that sets up a chef workstation'
+version '2.0.0'
+chef_version '>= 13.0'
+issues_url 'https://github.com/codenamephp/chef.cookbook.chef/issues'
+source_url 'https://github.com/codenamephp/chef.cookbook.chef'
+
+supports 'debian'
