2face2264f4ed2a68f3bf985f62d0f6338f65a9e
Bastian Schwarz <bastian@codename-php.de>
2020-06-27 23:22:15 +0200

    Removed the system stuff again in favor of their own cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,3 @@
 
 default['users'] = ['chef']
 default['codenamephp']['workstation_chef']['vscode']['extensions'] = ['chef-software.chef', 'eamodio.gitlens', 'github.vscode-pull-request-github']
-default['codenamephp']['workstation_chef']['locale'] = 'de_DE.UTF-8'
-default['codenamephp']['workstation_chef']['timezone'] = 'Europe/Berlin'
